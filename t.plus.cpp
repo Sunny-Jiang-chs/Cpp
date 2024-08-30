e68d94f4d45402f8a027afd46193cb6c870dd8ec
Sunny <1695787890@qq.com>
2024-08-30 11:19:13 +0800

    1

--- t.plus.cpp
+++ t.plus.cpp
@@ -2,10 +2,28 @@
 #include <iostream>
 
 using namespace std;
-
+struct arr{
+    int x;
+    int y;
+    inline void px(int a) { x += a;}
+};
+arr operator* (int n, arr a) { 
+    //arr c = a;
+    a.x = n * a.x;
+    a.y = n * a.y;
+    return a;
+}
+ostream& operator<< (ostream& o,arr a){
+    o << a.x << "," << a.y;
+    return o;
+}
 int main(){
-
-
-    
+    arr a;
+    a.x = 1;
+    a.y = 2;
+    a.px(3);
+    a = 3*a;
+    cout << a.x << "  " << a.y <<endl;
+    cout << a;
     return 0;
 }
